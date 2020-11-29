feec73dc9c0201cda8f444a0535901a96d947832
Ashley Hart <ashleyhart5618@gmail.com>
2020-11-29 18:53:57 -0500

    working with heroku

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
