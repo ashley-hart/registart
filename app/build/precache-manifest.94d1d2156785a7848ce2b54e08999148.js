71b999782e3bcc94ee1f785446ba016a7fea1ce3
Ashley Hart <ashleyhart5618@gmail.com>
2020-11-29 18:11:36 -0500

    working with heroku

---
+++ app/build/precache-manifest.94d1d2156785a7848ce2b54e08999148.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a867498a885e55acc788400d32e6cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b029a486c7aa59216c4",
+    "url": "/static/css/main.1e9559cd.chunk.css"
+  },
+  {
+    "revision": "9247b515cee55c237689",
+    "url": "/static/js/2.aecd31a7.chunk.js"
+  },
+  {
+    "revision": "0af762b46daa3c2facc099c515ce61cf",
+    "url": "/static/js/2.aecd31a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b029a486c7aa59216c4",
+    "url": "/static/js/main.77242c8b.chunk.js"
+  },
+  {
+    "revision": "297d694651322a6db170",
+    "url": "/static/js/runtime-main.ce42d076.js"
+  },
+  {
+    "revision": "5e49787ec6e2417af4fd1e9809ae6059",
+    "url": "/static/media/aboutmepic.5e49787e.jpg"
+  },
+  {
+    "revision": "47bc71e2a80c4a11e34bd0fbea300bce",
+    "url": "/static/media/logo2.47bc71e2.svg"
+  }
+]);
--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.listen(PORT, () => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-
+  app.use(express.static('app/build'));
 }
 
 // ROUTES
